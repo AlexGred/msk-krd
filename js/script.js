032ce3ab663d67fb10e196c4fb39dd1505ecac37
Alex Dolzhenkov <a;exgred.dev@gmail.com>
2016-10-15 19:07:39 +0300

    add slide form and html for services

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
 
   // Phone mask
   $('#phone-number').mask('+7 (999) 999-99-99');
+
+  // Slide contacts form
+  var slide = $('#slide-contact-form');
+  var slideClass = 'active-slide';
+
+  if (slide.hasClass(slideClass)) {
+    slide.removeClass(slideClass);
+  }
+  else {
+    slide.addClass(slideClass);
+  }
+
 });
